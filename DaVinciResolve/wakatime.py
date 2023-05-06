0994f39cb41951012520e36f6e3adaeafd56928c
ra100 <git@ra100.net>
2023-05-06 17:19:00 +0200

    ♻️ Update fusionscript loading

--- DaVinciResolve/wakatime.py
+++ DaVinciResolve/wakatime.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-import importlib
 import os
+import subprocess
 import sys
 import time
+
+sys.path.append('/opt/resolve/Developer/Scripting/Modules')
+
+import DaVinciResolveScript
 
 pluginName = "davinci-resolve-wakatime"
 version = "0.0.1"
@@ -10,29 +14,31 @@ version = "0.0.1"
 USER_HOME = os.path.expanduser("~")
 CONFIG_FILENAME = os.path.join(USER_HOME, ".wakatime.cfg")
 
-pathlib = '/opt/resolve/libs/Fusion/fusionscript.so'
-spec = importlib.util.spec_from_file_location('fusionscript', pathlib)
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
-resolve = module.scriptapp('Resolve')
+resolve = DaVinciResolveScript.scriptapp('Resolve')
 projectManager = resolve.GetProjectManager()
 
-project = projectManager.GetCurrentProject()
+def resolve_is_focused():
+  output = subprocess.check_output('xdotool getwindowfocus getwindowname', shell=True)
+  windowName = output.decode()
+  if "DaVinci Resolve" in windowName:
+    return True
+
+  return False
 
 while True:
-    if project is not None:
-        projectName = project.GetName()
-        currentPage = resolve.GetCurrentPage()
-        if currentPage is not None:
-            cmd = f"wakatime --project \"{projectName}\" \
-                            --entity-type domain \
-                            --entity \"{currentPage}\" \
-                            --plugin \"{pluginName}/{version}\"\
-                            --category designing \
-                            --config \"{CONFIG_FILENAME}\""
-            print(cmd)
-            os.system(cmd)
+    if resolve_is_focused():
+      project = projectManager.GetCurrentProject()
+      if project is not None:
+          projectName = project.GetName()
+          currentPage = resolve.GetCurrentPage()
+          if currentPage is not None:
+              cmd = f"wakatime --project \"{projectName}\" \
+                              --entity-type domain \
+                              --entity \"{currentPage}\" \
+                              --plugin \"{pluginName}/{version}\"\
+                              --category designing \
+                              --config \"{CONFIG_FILENAME}\""
+              print(cmd)
+              os.system(cmd)
 
-    # wait for 60 seconds before running the loop again
     time.sleep(30)
--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Tracks time spent on a project in a specific pate (cut, edit, color, ...) to wak
 ## Install Prerequisites
 
 - install `wakatime-cli` - https://github.com/wakatime/wakatime-cli
+- install `xdotool`
 - copy `DaVinciResolve/wakatime.py` to `~/.local/share/DaVinciResolve/Fusion/Scripts/Utility/wakatime.py`
 - run `Workspace` -> `Scripts` -> `wakatime`
