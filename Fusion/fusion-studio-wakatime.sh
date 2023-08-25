af106f099d8182f30e89067ca71cdf7a3f2f42fe
ra100 <git@ra100.net>
2023-08-25 09:17:21 +0200

    🐛 Fix Fusion start/skip

--- Fusion/fusion-studio-wakatime.sh
+++ Fusion/fusion-studio-wakatime.sh
@@ -6,7 +6,7 @@ if [ "$IDLE_TIME" -gt 15000 ]; then
   exit 0;
 fi
 
-FUSION_ACTIVE=$(xdotool getwindowfocus getwindowname | grep "Visual")
+FUSION_ACTIVE=$(xdotool getwindowfocus getwindowname | grep "Fusion")
 
 if [ -n "$FUSION_ACTIVE" ]; then
   /opt/BlackmagicDesign/Fusion18/fuscript -l lua "$HOME/.local/bin/fusion-studio-wakatime.lua"
